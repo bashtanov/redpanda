192bb54d3249a0dabccf3b5f13c90857ba51f5cf
Alexey Bashtanov <alexey.bashtanov@redpanda.com>
2025-07-09 13:00:38 +0100

    c/data-migrate/backend: separate partition-group map calculation

--- src/v/cluster/data_migration_backend.cc
+++ src/v/cluster/data_migration_backend.cc
@@ -1353,6 +1353,25 @@ ss::future<> backend::reconcile_existing_topic(
     }
 }
 
+backend::partition_consumer_group_map_t
+backend::build_migration_reconciliation_state(
+  const migration_metadata& metadata) const {
+    partition_consumer_group_map_t ret;
+    const auto& groups = std::visit(
+      [](const auto& migration) -> const chunked_vector<consumer_group>& {
+          return migration.groups;
+      },
+      metadata.migration);
+
+    for (const auto& group : groups) {
+        auto partition = _group_proxy.partition_for(group);
+        vassert(partition, "cannot get ntp for group {}", group);
+        auto [it, ins] = ret.try_emplace(*partition);
+        it->second.push_back(group);
+    }
+    return ret;
+}
+
 ss::future<> backend::reconcile_migration(
   migration_reconciliation_state& mrstate, const migration_metadata& metadata) {
     vlog(
@@ -1362,20 +1381,8 @@ ss::future<> backend::reconcile_migration(
       mrstate.scope.sought_state);
 
     if (!mrstate.partition_group_map) {
-        mrstate.partition_group_map.emplace();
-        const auto& groups = std::visit(
-          [](const auto& migration) -> const chunked_vector<consumer_group>& {
-              return migration.groups;
-          },
-          metadata.migration);
-
-        for (const auto& group : groups) {
-            auto partition = _group_proxy.partition_for(group);
-            vassert(partition, "cannot get ntp for group {}", group);
-            auto [it, ins] = mrstate.partition_group_map->try_emplace(
-              *partition);
-            it->second.push_back(group);
-        }
+        mrstate.partition_group_map.emplace(
+          build_migration_reconciliation_state(metadata));
     }
 
     co_await std::visit(
